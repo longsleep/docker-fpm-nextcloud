55f79df15ecab236ec1c7bd4c4d606eef75ddbdd
Simon Eisenmann <simon@struktur.de>
2016-08-16 14:32:43 +0200

    Do not recursivly change permissions

--- php-fpm.sh
+++ php-fpm.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-chown -R www-data:www-data /srv/www/config /srv/www/data /srv/www/apps
+
+set -e
+
+chgrp www-data /srv/www/config /srv/www/data /srv/www/apps
+chmod g+w /srv/www/config /srv/www/data /srv/www/apps
 exec /usr/sbin/php-fpm7.0 --nodaemonize --fpm-config /etc/php/7.0/fpm/php-fpm.conf
